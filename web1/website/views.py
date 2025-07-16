3077400f62eceec2b4cea52076e32f8bcba7ec05
Stqthis <efs.panagiotou@gmail.com>
2025-07-16 14:54:47 +0300

    fixed htmls

--- web1/website/views.py
+++ web1/website/views.py
@@ -11,6 +11,6 @@ def home():
 def Stathis_presentation():
     return render_template('Stathis_present.html')
 
-@views.route('/sign-up')
+@views.route('/TodoList')
 def sign_up():
-    return render_template('signUp.html',name="stathis")
+    return render_template('TodoList.html',name="stathis")
